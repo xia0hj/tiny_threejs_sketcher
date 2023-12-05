b3a88e9163da1516e767551d2ff21285789b44b0
xia0hj <719887163@qq.com>
2023-12-05 21:51:44 +0800

    🥱 unstable_batchedUpdates

--- src/feature/global_state/zustand_impl.ts
+++ src/feature/global_state/zustand_impl.ts
@@ -4,6 +4,7 @@ import {
   GlobalStateHook,
   GlobalStateSetter,
 } from "@/feature/global_state/interface";
+import { unstable_batchedUpdates } from "react-dom";
 import { useStore } from "zustand";
 import { createStore } from "zustand/vanilla";
 
@@ -16,7 +17,11 @@ export const getGlobalState: GlobalStateGetter = (key) => {
 };
 
 export const setGlobalState: GlobalStateSetter = (key, value) => {
-  store.setState({ [key]: value });
+  /**
+   * wrapped by unstable_batchedUpdates
+   * -> https://github.com/pmndrs/zustand/issues/302 
+   */
+  unstable_batchedUpdates(() => store.setState({ [key]: value }));
 };
 
 export const useGlobalState: GlobalStateHook = (key) => [
