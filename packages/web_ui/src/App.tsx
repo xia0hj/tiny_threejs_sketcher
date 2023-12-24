3f017d9522d61fabadc26a11250afd2923ca72b3
xia0hj <719887163@qq.com>
2023-12-25 00:48:31 +0800

    👆 tsup

--- packages/web_ui/src/App.tsx
+++ packages/web_ui/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { add } from "sdk";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => add(count ,1))}>
           count is {count}
         </button>
         <p>
