b0bd9dcf7b115e6b4b39f085e97630b2606c8889
xia0hj <719887163@qq.com>
2024-02-09 00:46:24 +0800

    ✨ tailwindcss + nextui

--- packages/web_ui/src/component/App/index.tsx
+++ packages/web_ui/src/component/App/index.tsx
@@ -1,12 +1,15 @@
 import { useSceneRenderer } from "@src/component/App/useSceneRenderer";
-import style from "@src/component/App/style.module.css";
+import { Button } from "@nextui-org/react";
 
 export const App = () => {
   const canvasRef = useSceneRenderer();
 
   return (
-    <div className={style.canvas_container}>
-      <canvas className={style.canvas_threejs} ref={canvasRef} />
+    <div className="w-full flex h-dvh">
+      <div className="w-1/4 h-dvh">
+        <Button>啊啊啊</Button>
+      </div>
+      <canvas className="w-3/4 h-dvh block" ref={canvasRef} />
     </div>
   );
 };
--- packages/web_ui/src/main.tsx
+++ packages/web_ui/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "normalize.css";
 import "@src/base.css";
 import { App } from "@src/component/App";
+import { NextUIProvider } from "@nextui-org/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>,
 );
