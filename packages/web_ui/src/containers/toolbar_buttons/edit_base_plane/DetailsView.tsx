974967e8113e6f2f919b2de733ac6c8e40281777
xia0hj <719887163@qq.com>
2024-08-01 22:23:07 +0800

    🎏 edit base plane

--- packages/web_ui/src/containers/toolbar_buttons/edit_base_plane/DetailsView.tsx
+++ packages/web_ui/src/containers/toolbar_buttons/edit_base_plane/DetailsView.tsx
@@ -5,21 +5,36 @@ import {
   LogoutOutlined,
 } from "@ant-design/icons";
 import { Toolbar, ToolbarButton } from "@src/components/toolbar";
+import { useSketcherStore } from "@src/store";
+import { COMMAND_KEY } from "tiny_threejs_sketcher";
 
 export const DetailsView: ToolbarButton["DetailsView"] = ({ exit }) => {
+
+  const tinyThreejsSketcher = useSketcherStore(state => state.tinyThreejsSketcher);
+
   const editor2dButtons: ToolbarButton[] = [
     {
       label: "保存",
       icon: <CheckOutlined />,
       onClick() {
-        exit();
+        const res = tinyThreejsSketcher?.runCommand(COMMAND_KEY.stop_edit_base_plane);
+        if (res === true) {
+          exit();
+          return true;
+        }
+        return false;
       },
     },
     {
       label: "退出",
       icon: <CloseOutlined />,
       onClick() {
-        exit();
+        const res = tinyThreejsSketcher?.runCommand(COMMAND_KEY.stop_edit_base_plane);
+        if (res === true) {
+          exit();
+          return true;
+        }
+        return false;
       },
     },
   ];
